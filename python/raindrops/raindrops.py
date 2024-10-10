8bcee9109a54c9a9943f4791c445c13527c418e7
Ken Jensen <ken.jensen@ookla.com>
2024-10-10 15:08:07 -0600

    Updated raindrop sounds

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,2 +1,17 @@
+"""Functions convert a number into its corresponding raindrop sounds
+
+"""
 def convert(number):
-    pass
+    """Convert a number into its corresponding raindrop sounds
+
+    :param number: int.
+    :return: str - raindrop sounds
+    """
+    raindrop_sounds = ""
+    if number % 3 == 0:
+        raindrop_sounds += "Pling"
+    if number % 5 == 0:
+        raindrop_sounds += "Plang"
+    if number % 7 == 0:
+        raindrop_sounds += "Plong"
+    return raindrop_sounds or str(number)
