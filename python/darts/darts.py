ecf510cad873e0bef0e75f6944b77cd428c71705
Ken Jensen <ken.jensen@ookla.com>
2024-10-11 11:34:01 -0600

    Finshed Darts

--- python/darts/darts.py
+++ python/darts/darts.py
@@ -1,2 +1,21 @@
+"""Calculate the points scored in a single toss of a Darts game.
+
+"""
 def score(x, y):
-    pass
+    """Calculate the points scored in a single toss of a Darts game.
+    with out import math module.
+
+    :param x: int, the x coordinate of the dart.
+    :param y: int, the y coordinate of the dart.
+    :return: int, the points scored in a single toss of a Darts game.
+
+    """
+    distance = ((x - 0)**2 + (y - 0)**2)**0.5
+    if distance <= 1:
+        return 10
+    elif distance <= 5:
+        return 5
+    elif distance <= 10:
+        return 1
+    else:
+        return 0
