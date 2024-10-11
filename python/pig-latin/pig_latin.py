389cb99ea23df644bda867bd4477bc5f55f70ffe
Ken Jensen <ken.jensen@ookla.com>
2024-10-11 10:58:15 -0600

    Updated Pig latin

--- python/pig-latin/pig_latin.py
+++ python/pig-latin/pig_latin.py
@@ -1,2 +1,21 @@
+"""translate text from English to Pig Latin
+
+"""
 def translate(text):
-    pass
+    """Function to translate English to Pig Latin and return the results.
+
+    :param text: Input text for translation
+    :return: Translated text
+    """
+    words = text.split()
+    updated_text = []
+    for word in words:
+        if word[0] in "aeiou" or word[:2] in ["yt", "xr"]:
+            updated_text.append(word + "ay")
+        elif word[:3] in ["thr", "sch", "squ"]:
+            updated_text.append(word[3:] + word[:3] + "ay")
+        elif word[:2] in ["ch", "qu", "th", "rh"]:
+            updated_text.append(word[2:] + word[:2] + "ay")
+        else:
+            updated_text.append(word[1:] + word[0] + "ay")
+    return " ".join(updated_text)
