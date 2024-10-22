fd62144ec6ce4aba5e069cbfd7e2ff4c0aadcccd
Ken Jensen <ken.jensen@ookla.com>
2024-10-22 11:57:11 -0600

    Code cleaned up and Lint is good

--- python/isbn-verifier/isbn_verifier.py
+++ python/isbn-verifier/isbn_verifier.py
@@ -8,43 +8,26 @@ def is_valid(isbn):
     :param isbn: str - What we what to check
     :return: bool - True if the string is a ISBN-10
     """
-    print(f"{isbn=}")
     # Clean the string and remove hyphens
     isbn = isbn.replace("-", "")
-    print(f"{isbn=}")
 
-    # Check if the string is 10 characters long
-    print(f"{len(isbn)=}")
+    # Make sure there are 10 characters in the string
     if len(isbn) != 10:
         return False
 
     # Check if the first 9 characters are numbers
-    print(f"{isbn[:-1].isdigit()=}")
-    print(f"first 9 {isbn[:-1]=}")
     if not isbn[:-1].isdigit():
         return False
 
-    # Check if the last character is a number or X
-    print(f"{isbn[-1].isdigit()=}")
-    print(f"{isbn[-1]=}")
-    if not isbn[-1].isdigit() and isbn[-1] != "X":
+    # Check that the last character is an 'X' or a number
+    if isbn[-1] != "X" and not isbn[-1].isdigit():
         return False
 
-    # find the sum of the first 9 characters
-    sum_ = 0
-    for i in range(9):
-        sum_ += int(isbn[i]) * (10 - i)
-        print(f"{i} {isbn[i]=} {sum_=}")
-    if isbn[-1] == "X":
-        sum_ += 10
-        print(f"9   X   {sum_=}")
-    else:
-        sum_ += int(isbn[-1])
-        print(f"9 {int(isbn[-1])}  {sum_=}")
+    # Calculate the sum of the first 9 characters
+    sum_ = sum(int(isbn[i]) * (10 - i) for i in range(9))
 
+    # Add the value of the last character
+    sum_ += 10 if isbn[-1] == "X" else int(isbn[-1])
 
-    print(f"Main Test for Mod {sum_ % 11=}")
-
-    if sum_ % 11 == 0:
-        return True
-    return False
+    # Final check that the sum is divisible by 11
+    return sum_ % 11 == 0
