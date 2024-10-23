5417edaee3a7c3660869df08abf7ae6cce3c29c7
Ken Jensen <ken.jensen@ookla.com>
2024-10-23 09:27:51 -0600

    Update code and reAdd PyLint

--- python/resistor-color/resistor_color.py
+++ python/resistor-color/resistor_color.py
@@ -1,6 +1,41 @@
-def color_code(color):
-    pass
+"""
+Look up the numerical value associated with a Resistor's band colors.
+"""
 
+def color_code(color):
+    """
+    Return the numerical value associated with a Resistor's band color.
+    color: str  ->  The color of the band.
+    return: int  ->  The numerical value associated with the band color.
+    """
+    resistor_color_index = {
+        "black": 0,
+        "brown": 1,
+        "red": 2,
+        "orange": 3,
+        "yellow": 4,
+        "green": 5,
+        "blue": 6,
+        "violet": 7,
+        "grey": 8,
+        "white": 9
+    }
+    return resistor_color_index[color]
 
 def colors():
-    pass
+    """
+    Returns the full list resister colors bands.
+    """
+    resistor_colors = [
+        "black",
+        "brown",
+        "red",
+        "orange",
+        "yellow",
+        "green",
+        "blue",
+        "violet",
+        "grey",
+        "white"
+    ]
+    return resistor_colors
