b19daee6a947445bae997b87bf6d34fa3d76e47c
Ken Jensen <ken.jensen@ookla.com>
2024-11-14 06:34:13 -0700

    Finshed Lexiconia

--- python/etl/etl.py
+++ python/etl/etl.py
@@ -1,2 +1,16 @@
+"""
+Lexiconia
+"""
+
+
 def transform(legacy_data):
-    pass
+    """
+    change the data format of letters and their point values in the game.
+
+    :param legacy_data: dict - a dictionary of letters and their point values.
+    :return: dict - a dictionary of letters and their point values.
+    """
+    new_data = {}
+    for key, value in legacy_data.items():
+        new_data.update({letter.lower(): key for letter in value})
+    return new_data
