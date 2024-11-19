dc4c84f6209bac5880329565cfe4e33309c1818a
Ken Jensen <ken.jensen@ookla.com>
2024-11-19 10:42:29 -0700

    Finshed with Pythagorean Triplet

--- python/pythagorean-triplet/pythagorean_triplet.py
+++ python/pythagorean-triplet/pythagorean_triplet.py
@@ -1,2 +1,21 @@
+"""
+Pythagorean Triplet
+"""
+
+
 def triplets_with_sum(number):
-    pass
+    """
+    pythagorean triplets with a sum equal to that number.
+
+    :param number: int - The number to find Pythagorean triplets for.
+    :return: list - A list of Pythagorean triplets.
+    """
+    triplets = []
+    for a in range(1, number):
+        for b in range(a, number):
+            c = number - a - b
+            if c < b:
+                break
+            if a ** 2 + b ** 2 == c ** 2:
+                triplets.append([a, b, c])
+    return triplets
