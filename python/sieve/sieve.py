8c1be8129c1491a8558da4ab59c6f3fa9e1408f6
Ken Jensen <ken.jensen@ookla.com>
2024-11-19 09:52:52 -0700

    Finshed Sieve of Eratosthenes

--- python/sieve/sieve.py
+++ python/sieve/sieve.py
@@ -1,2 +1,22 @@
+"""
+Sieve of Eratosthenes
+"""
+
+
 def primes(limit):
-    pass
+    """
+    Calculates the primes up to a given number.
+
+    :param limit: int - The limit to calculate the primes up to.
+    :return: list - A list of primes.
+    """
+    primes_list = []
+    multiples = set()
+
+    for index in range(2, limit + 1):
+        # Check that we have not already added this prime number
+        if index not in multiples:
+            primes_list.append(index)
+            multiples.update(range(index * index, limit + 1, index))
+
+    return primes_list
