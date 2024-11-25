3cabfcb4ef1919a5efc6c085bca0de56cc0168b2
Ken Jensen <ken.jensen@ookla.com>
2024-11-25 09:11:21 -0700

    Finshed Proverb

--- python/proverb/proverb.py
+++ python/proverb/proverb.py
@@ -1,2 +1,24 @@
-def proverb():
-    pass
+"""
+Proverb
+"""
+
+
+def proverb(*args, qualifier=None):
+    """
+    Generate the full text of the proverb
+
+    :return: str - The full text of the proverb
+    """
+    result = []
+    if len(args) == 0:
+        return result
+
+    for i in range(len(args) - 1):
+        result.append(f"For want of a {args[i]} the {args[i + 1]} was lost.")
+
+    if qualifier:
+        result.append(f"And all for the want of a {qualifier} {args[0]}.")
+    else:
+        result.append(f"And all for the want of a {args[0]}.")
+
+    return result
