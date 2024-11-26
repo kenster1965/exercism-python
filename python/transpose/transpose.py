546cadd2385a045e8e8776ecfd193bd43e61788c
Ken Jensen <ken.jensen@ookla.com>
2024-11-26 10:57:08 -0700

    Thanks to Warren for the help marked as done

--- python/transpose/transpose.py
+++ python/transpose/transpose.py
@@ -1,2 +1,24 @@
+"""
+Transpose
+"""
+
+
 def transpose(text):
-    pass
+    """
+    Transpose the input text
+
+    :param text: str - The text to transpose
+    :return: str - The transposed text
+    """
+    lines = text.split("\n")
+    max_length = max(len(line) for line in lines)
+    padded_lines = [line.ljust(max_length) for line in lines]
+    transposed_lines = ["".join(line) for line in zip(*padded_lines)]
+
+    trimmed_lines = []
+    min_length = 0
+    for line in reversed(transposed_lines):
+        min_length = max(min_length, len(line.rstrip()))
+        trimmed_lines.append(line[:min_length])
+
+    return "\n".join(reversed(trimmed_lines))
