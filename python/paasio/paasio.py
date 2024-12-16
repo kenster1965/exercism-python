46aecda79b300caa52580dd589296fa4716f8f2a
Ken Jensen <ken.jensen@ookla.com>
2024-12-16 12:05:31 -0700

    Hard one but all tests are passing.

--- python/paasio/paasio.py
+++ python/paasio/paasio.py
@@ -1,10 +1,10 @@
+"""This module provides classes for metering I/O operations on files and sockets."""
 import io
-import socket
 import errno
 import os
-class MeteredFile(io.BufferedRandom):
-    """Implement using a subclassing model."""
 
+class MeteredFile(io.BufferedRandom):
+    """With subclassing."""
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._read_bytes = 0
@@ -13,27 +13,29 @@ class MeteredFile(io.BufferedRandom):
         self._write_ops = 0
 
     def __enter__(self):
-        # Support context management
+        """Return self to allow usage like `with MeteredFile(...) as f:`."""
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        # Delegate __exit__ to the wrapped object
+        """Return the result of the superclass's __exit__ method."""
         return super().__exit__(exc_type, exc_val, exc_tb)
 
     def __iter__(self):
-        # Return self for iteration
+        """Return self to allow usage like `for line in f:`."""
         return self
 
     def __next__(self):
-        # Read the next line using readline
-        line = self.readline()
-        if line == b"":  # End of file
-            raise StopIteration
-        self._read_bytes += len(line)
+        """Return the next line from the file."""
+        data = super().readline()
+        self._read_bytes += len(data)
         self._read_ops += 1
-        return line
+        if data:
+            return data
+        else:
+            raise StopIteration
 
     def read(self, size=-1):
+        """Read up to size bytes from the file."""
         data = super().read(size)
         self._read_bytes += len(data)
         self._read_ops += 1
@@ -41,28 +43,35 @@ class MeteredFile(io.BufferedRandom):
 
     @property
     def read_bytes(self):
+        """Return the number of bytes read from the file."""
         return self._read_bytes
 
     @property
     def read_ops(self):
+        """Return the reading of the file."""
         return self._read_ops
 
     def write(self, b):
-        written = super().write(b)
-        self._write_bytes += written
+        """Write the given bytes to the file."""
+        write_len = super().write(b)
+        self._write_bytes += write_len
         self._write_ops += 1
-        return written
+        return write_len
 
     @property
     def write_bytes(self):
+        """Return the number of bytes written to the file."""
         return self._write_bytes
 
     @property
     def write_ops(self):
+        """Return the number of write operations."""
         return self._write_ops
 
 class MeteredSocket:
+    """MeteredSocket` class."""
     def __init__(self, socket):
+        """Initialize the MeteredSocket object."""
         self._socket = socket
         self._closed = False
         self._recv_ops = 0
@@ -71,30 +80,37 @@ class MeteredSocket:
         self._send_bytes = 0
 
     def __enter__(self):
+        """Return self to allow usage like `with MeteredSocket(...) as s:`."""
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
+        """Return the result of the wrapped socket's __exit__ method."""
         self._closed = True
         # Call the wrapped socket's __exit__ method
         return self._socket.__exit__(exc_type, exc_val, exc_tb)
 
     @property
     def recv_ops(self):
+        """Return the number of receive operations."""
         return self._recv_ops
 
     @property
     def recv_bytes(self):
+        """Return the number of bytes received."""
         return self._recv_bytes
 
     @property
     def send_ops(self):
+        """Return the number of send operations."""
         return self._send_ops
 
     @property
     def send_bytes(self):
+        """Return the number of bytes sent."""
         return self._send_bytes
 
     def recv(self, bufsize, flags=0):
+        """Receive data from the socket."""
         if self._closed:
             raise OSError(errno.EBADF, os.strerror(errno.EBADF))
         if not isinstance(flags, int):
@@ -105,6 +121,7 @@ class MeteredSocket:
         return data
 
     def send(self, data, flags=0):
+        """Send data to the socket."""
         if self._closed:
             raise OSError(errno.EBADF, os.strerror(errno.EBADF))
         if not isinstance(flags, int):
