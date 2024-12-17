6938952c32e802a406b4a3a644fb519793d0493c
Ken Jensen <ken.jensen@ookla.com>
2024-12-17 12:16:09 -0700

    Finshed Matrix, Starting High Scores

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,9 +1,30 @@
+"""Matrix"""
+
 class Matrix:
+    """Matrix Class"""
     def __init__(self, matrix_string):
-        pass
+        """
+        Setup the matrix from a string.
+        :param matrix_string: A string representation of the matrix.
+        """
+        # Convert the matrix string into a list of lists
+        self._matrix = [
+            [int(num) for num in row.split()]
+            for row in matrix_string.splitlines()
+        ]
 
     def row(self, index):
-        pass
+        """
+        Get the row from the index.
+        :param index: The index.
+        :return: A list of the row.
+        """
+        return self._matrix[index - 1]
 
     def column(self, index):
-        pass
+        """
+        Get the column from the index.
+        :param index: The index.
+        :return: A list opf the column.
+        """
+        return [row[index - 1] for row in self._matrix]
