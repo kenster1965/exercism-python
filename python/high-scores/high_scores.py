0a7c9df885566dfa7e95035f5d7f2744603e0d5e
Ken Jensen <ken.jensen@ookla.com>
2024-12-18 08:04:04 -0700

    Finshed High Scores

--- python/high-scores/high_scores.py
+++ python/high-scores/high_scores.py
@@ -1,3 +1,22 @@
+"""High Scores"""
+
+
 class HighScores:
+    """High Scores"""
     def __init__(self, scores):
-        pass
+        """Initialize the HighScores.
+        :param scores: A list of scores.
+        """
+        self.scores = scores
+
+    def latest(self):
+        """Get the latest score."""
+        return self.scores[-1]
+
+    def personal_best(self):
+        """Get the personal best score."""
+        return max(self.scores)
+
+    def personal_top_three(self):
+        """Get the personal top three scores."""
+        return sorted(self.scores, reverse=True)[:3]
