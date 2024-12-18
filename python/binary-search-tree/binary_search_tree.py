a257e20907c6d0fbece045ea18f208cdd9d45e10
Ken Jensen <ken.jensen@ookla.com>
2024-12-18 13:58:24 -0700

    Finshed Binary Tree Search

--- python/binary-search-tree/binary_search_tree.py
+++ python/binary-search-tree/binary_search_tree.py
@@ -1,19 +1,58 @@
+"""Binary Search Tree"""
+
+
 class TreeNode:
+    """Node of a binary search tree."""
     def __init__(self, data, left=None, right=None):
-        self.data = None
-        self.left = None
-        self.right = None
+        self.data = data
+        self.left = left
+        self.right = right
 
     def __str__(self):
+        """Return a string representation of the TreeNode."""
         return f'TreeNode(data={self.data}, left={self.left}, right={self.right})'
 
-
 class BinarySearchTree:
+    """Binary Search Tree"""
     def __init__(self, tree_data):
-        pass
+        """Set Binary Search Tree."""
+        self.root = None
+        for value in tree_data:
+            self.add(value)
+
+    def add(self, value):
+        """Add a value."""
+        if self.root is None:
+            self.root = TreeNode(value)
+        else:
+            self._add_recursive(self.root, value)
+
+    def _add_recursive(self, node, value):
+        """Recursively add a value."""
+        if value <= node.data:
+            if node.left is None:
+                node.left = TreeNode(value)
+            else:
+                self._add_recursive(node.left, value)
+        else:
+            if node.right is None:
+                node.right = TreeNode(value)
+            else:
+                self._add_recursive(node.right, value)
 
     def data(self):
-        pass
+        """Return the root node."""
+        return self.root
 
     def sorted_data(self):
-        pass
+        """Returns the data."""
+        result = []
+        self._in_order_traversal(self.root, result)
+        return result
+
+    def _in_order_traversal(self, node, result):
+        """Helper method for in-order traversal of the tree."""
+        if node is not None:
+            self._in_order_traversal(node.left, result)
+            result.append(node.data)
+            self._in_order_traversal(node.right, result)
