7fd540528dae38e54b0018120ae20ebd645fb17e
Ken Jensen <ken.jensen@ookla.com>
2025-01-06 15:27:03 -0700

    Finshed Hamming and starting Flatten

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,2 +1,16 @@
+"""Hamming distance"""
+
 def distance(strand_a, strand_b):
-    pass
+    """
+    Calculate the Hamming distance between two DNA strands.
+
+    :param strand_a: str: First DNA strand.
+    :param strand_b: str: Second DNA strand.
+    :return: int: The Hamming distance.
+    :raises ValueError: If the strands are of different lengths.
+    """
+    if len(strand_a) != len(strand_b):
+        raise ValueError("Strands must be of equal length.")
+
+    # Calculate the 'Hamming distance'
+    return sum(1 for a, b in zip(strand_a, strand_b) if a != b)
