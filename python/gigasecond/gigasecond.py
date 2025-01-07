f2b519cce604eee019decfaafac715d17e8793a3
Ken Jensen <ken.jensen@ookla.com>
2025-01-07 12:39:49 -0700

    finsghed gigasecond starting 2-fer

--- python/gigasecond/gigasecond.py
+++ python/gigasecond/gigasecond.py
@@ -1,2 +1,13 @@
+"""Gigasecond"""
+from datetime import datetime, timedelta
+
+
 def add(moment):
-    pass
+    """
+    Add one gigasecond (10^9 seconds) to the given datetime.
+
+    :param moment: datetime: The input datetime.
+    :return: datetime: The datetime after adding one gigasecond.
+    """
+    gigasecond = 10**9  # A gigasecond in seconds
+    return moment + timedelta(seconds=gigasecond)
