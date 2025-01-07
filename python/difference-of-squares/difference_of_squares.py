0a903bc47a23bf64fdf7cf49a8b07ce4e567f526
Ken Jensen <ken.jensen@ookla.com>
2025-01-07 08:08:58 -0700

    Finshed square numbers

--- python/difference-of-squares/difference_of_squares.py
+++ python/difference-of-squares/difference_of_squares.py
@@ -1,10 +1,34 @@
+"""Difference of Squares"""
+
+
 def square_of_sum(number):
-    pass
+    """
+    The square of the sum of the first n natural numbers.
+    :param number: int
+    :return: int
+    """
+    total = 0
+    for i in range(1, number+1):
+        total += i
+    return total ** 2
 
 
 def sum_of_squares(number):
-    pass
+    """
+    The sum of the squares of the first n natural numbers.
+    :param number: int
+    :return: int
+    """
+    total = 0
+    for i in range(1, number+1):
+        total += i * i
+    return total
 
 
 def difference_of_squares(number):
-    pass
+    """
+    The difference between the square of the sum and the sum of the squares.
+    :param number: int
+    :return: int
+    """
+    return square_of_sum(number) - sum_of_squares(number)
