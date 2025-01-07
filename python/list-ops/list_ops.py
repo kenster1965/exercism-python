e1ebcb752981ed23eb388acdc34cb1cf841667dc
Ken Jensen <ken.jensen@ookla.com>
2025-01-07 10:06:45 -0700

    complete lists

--- python/list-ops/list_ops.py
+++ python/list-ops/list_ops.py
@@ -1,30 +1,45 @@
+"""List Ops"""
+
+
 def append(list1, list2):
-    pass
+    """Append two lists"""
+    return list1 + list2
 
 
 def concat(lists):
-    pass
+    """Concatenate a list of lists"""
+    return [item for sublist in lists for item in sublist]
 
 
-def filter(function, list):
-    pass
+def filter(function, the_list):
+    """Filter a list"""
+    return [item for item in the_list if function(item)]
 
 
-def length(list):
-    pass
+def length(the_list):
+    """Return the length of a list"""
+    return len(the_list)
 
 
-def map(function, list):
-    pass
+def map(function, the_list):
+    """Map a function to a list"""
+    return [function(item) for item in the_list]
 
 
-def foldl(function, list, initial):
-    pass
+def foldl(function, the_list, initial):
+    """Fold a function over a list from the left"""
+    if not the_list:
+        return initial
+    return foldl(function, the_list[1:], function(initial, the_list[0]))
 
 
-def foldr(function, list, initial):
-    pass
+def foldr(function, the_list, initial):
+    """Fold a function over a list from the right"""
+    for item in the_list[::-1]:
+        initial = function(initial, item)
+    return initial
 
 
-def reverse(list):
-    pass
+def reverse(the_list):
+    """Reverse a list"""
+    return the_list[::-1]
