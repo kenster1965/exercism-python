2d506f2c7e0f9cb4c212d5b84f2e4540afe3603b
Ken Jensen <ken.jensen@ookla.com>
2025-01-09 10:51:27 -0700

    Finshed Primes starting Robot

--- python/nth-prime/nth_prime.py
+++ python/nth-prime/nth_prime.py
@@ -1,2 +1,17 @@
+"""Primes"""
+
+
 def prime(number):
-    pass
+    """Returns the nth prime number"""
+    if number < 1:
+        raise ValueError("there is no zeroth prime")
+    primes = []
+    candidate = 2
+    while len(primes) < number:
+        for prime in primes:
+            if candidate % prime == 0:
+                break
+        else:
+            primes.append(candidate)
+        candidate += 1
+    return primes[-1]
