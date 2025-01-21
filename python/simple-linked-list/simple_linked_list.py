d4d5e43c5f95d59eb647f25835a7976bcdc1791a
Ken Jensen <ken.jensen@ookla.com>
2025-01-21 14:46:18 -0700

    Finshed Linked list

--- python/simple-linked-list/simple_linked_list.py
+++ python/simple-linked-list/simple_linked_list.py
@@ -1,36 +1,77 @@
+"""Simple Linked List"""
 class EmptyListException(Exception):
-    pass
-
+    """Empty List Exception"""
 
 class Node:
-    def __init__(self, value):
-        pass
+    """Node class"""
+    def __init__(self, value, next_node=None):
+        """Node
+            value: value of the node
+            next_node: next node
+        """
+        self._value = value
+        self._next = next_node
 
     def value(self):
-        pass
+        """Return the value of the node"""
+        return self._value
 
     def next(self):
-        pass
+        """Return the next node"""
+        return self._next
+
+    def __str__(self):
+        """Return the string representation of the node"""
+        return f'Node({self._value}, {self._next})'
+
+    def __repr__(self):
+        """Return the string representation of the node"""
+        return f'Node({self._value}, {self._next})'
 
 
 class LinkedList:
+    """Linked List"""
     def __init__(self, values=None):
-        pass
+        """Linked List
+            values: list of values
+        """
+        self._head = None
+        self._len = 0
+        if values:
+            for value in values:
+                self.push(value)
 
     def __iter__(self):
-        pass
+        """Return the iterator of the linked list"""
+        while self._head:
+            yield self.pop()
 
     def __len__(self):
-        pass
+        """Return the length of the linked list"""
+        return self._len
 
     def head(self):
-        pass
+        """Return the head of the linked list"""
+        if self._head:
+            return self._head
+        else:
+            raise EmptyListException('The list is empty.')
 
     def push(self, value):
-        pass
+        """Push a value to the linked list"""
+        node = Node(value, self._head)
+        self._head = node
+        self._len += 1
 
     def pop(self):
-        pass
+        """Pop a value from the linked list"""
+        current_head = self._head
+        self._head = self.head().next()
+        self._len -= 1
+        return current_head.value()
 
     def reversed(self):
-        pass
+        """Return the reversed linked list"""
+        values = list(self)
+        values.reverse()
+        return values
